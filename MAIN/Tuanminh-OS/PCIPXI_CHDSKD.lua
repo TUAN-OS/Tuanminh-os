8ce4d7a79feb867969470b75992d4b6e91b1420b
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-10-01 10:34:50 +0700

    Add files via upload

---
+++ MAIN/Tuanminh-OS/PCIPXI_CHDSKD.lua
@@ -0,0 +1,14 @@
+local PXI
+print ("Starting tuanminh-OS")
+print ("PCI/PXI: test good")
+print ("waiting system checking os-FILE")
+print ("good")
+function FINDFILE == requied findingfile,fileexplorer:connect("authencation==SYSTEM"),("%PATHDIR%")
+print ("loading")
+if %PATHDIR% notexist==true then
+  print("In function ('FINDFILE'), the code is missing or need to recoding")
+  print("BOOTERROR: NOT_CODING_EXACTLY")
+elseif %PATHDIR% exist==true or notexist==false then
+  print("booting to SYSTEM.EXE")
+  call system.exe
+end
---
+++ MAIN/Tuanminh-OS/background.lua
@@ -0,0 +1,13 @@
+Local function background
+local null
+  Set.BackgroundApplication=C:\MAIN\Tuanminh-OS\lib\bck1.png
+  Set.BackgroundAlivable=C:\MAIN\Tuanminh-OS\lib
+  if null==BCK1.png then
+    print("Default background set")
+    end
+  elseif null == fish.PNG then
+    Set.BackgroundApplication=C:\MAIN\Tuanminh-OS\lib
+    print ("Fish wallpaper? it cool!")
+    end
+  end
+end
